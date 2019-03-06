b86937d33dbe2f44eba39f8260e88aa296f7a5b8
William Young <mr.william.a.young@gmail.com>
2019-03-06 00:08:21 -0500

    initial commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "spark-experiments"
+
+version := "0.1"
+
+scalaVersion := "2.12.2"
+
+val sparkVersion = "2.4.0"
+
+scalacOptions += "-Ypartial-unification"
+
+resolvers += Resolver.sonatypeRepo("releases")
+
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.9")
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.typelevel" %% "cats-core" % "1.6.0"
+)
---
+++ src/main/scala/com/example/Main.scala
@@ -0,0 +1,37 @@
+package com.example
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+
+object Main extends SparkEnv {
+
+  import spark.implicits._
+
+  def test1() = {
+    val df = spark.read.json("src/main/scala/com/example/resources/months.json")
+    @transient val w1 = Window.orderBy("monthNumber")
+    val indexedDF = df.withColumn("monthNumber", monotonically_increasing_id() + 1)
+    val lagIndexDF = indexedDF
+      .where(!$"flag")
+      .withColumn("prevFalseMonthNumber", lag("monthNumber", 1, 0).over(w1))
+    val resultDF = indexedDF.as("indexedDF")
+      .join(lagIndexDF.as("lagIndexDF"), $"indexedDF.monthNumber" > $"lagIndexDF.prevFalseMonthNumber" && $"indexedDF.monthNumber" <= $"lagIndexDF.monthNumber", "left")
+      .select($"indexedDF.month", $"lagIndexDF.month".as("nextOrCurrentFalse"))
+      .orderBy($"indexedDF.monthNumber".asc)
+    resultDF.show
+  }
+
+  def test2() = {
+    val df = spark.read.parquet("src/main/scala/com/example/resources/ts_data")
+    val q = df.select("timestamp").where($"timestamp" > "2018-12-01 18:00:00")
+    q.explain(true)
+    df.printSchema()
+    df.show
+    q.show
+  }
+
+  def main(args: Array[String]) = {
+    test2()
+  }
+}
