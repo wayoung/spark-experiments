1b912ee63bc310f57402a7cce2066cfc322a7727
William Young <williamy@spotify.com>
2020-10-05 18:03:10 -0400

    clean up code

--- demo/src/main/scala/com/example/demo/Main.scala
+++ demo/src/main/scala/com/example/demo/Main.scala
@@ -11,29 +11,6 @@ object Main extends SparkEnv {
 
   import spark.implicits._
 
-  def test1() = {
-    val df = spark.read.json("src/main/scala/com/example/resources/months.json")
-    @transient val w1 = Window.orderBy("monthNumber")
-    val indexedDF = df.withColumn("monthNumber", monotonically_increasing_id() + 1)
-    val lagIndexDF = indexedDF
-      .where(!$"flag")
-      .withColumn("prevFalseMonthNumber", lag("monthNumber", 1, 0).over(w1))
-    val resultDF = indexedDF.as("indexedDF")
-      .join(lagIndexDF.as("lagIndexDF"), $"indexedDF.monthNumber" > $"lagIndexDF.prevFalseMonthNumber" && $"indexedDF.monthNumber" <= $"lagIndexDF.monthNumber", "left")
-      .select($"indexedDF.month", $"lagIndexDF.month".as("nextOrCurrentFalse"))
-      .orderBy($"indexedDF.monthNumber".asc)
-    resultDF.show
-  }
-
-  def test2() = {
-    val df = spark.read.parquet("data/generated/ts_data")
-    val q = df.select("timestamp").where($"part" > 50 && $"timestamp" > "2018-12-01 18:00:00")
-    q.explain(true)
-    df.printSchema()
-    df.show
-    q.show
-  }
-
   def generateTimestampData(start: Long, n: Int): DataFrame = {
     val rnd = scala.util.Random
     val rdd: RandomRDD[Row] = new RandomRDD(spark.sparkContext, n, spark.sparkContext.statusTracker.getExecutorInfos.length, Row(rnd.nextInt(3).toLong))
@@ -44,17 +21,6 @@ object Main extends SparkEnv {
       .withColumn("timestamp", from_unixtime(sum($"timestamp").over(upToCurrentWindow) + start))
       .withColumn("eventID", concat(typedLit("event"), row_number().over(orderedWindow)))
       .withColumn("part", floor(row_number().over(orderedWindow) / 1000))
-  }
-
-  def writeGeneratedData(path: String, n: Int): Unit = {
-    generateTimestampData(java.sql.Timestamp.valueOf("2018-12-01 09:00:00").getTime / 1000L, n)
-      .repartition(27)
-      .write
-      .partitionBy("part")
-      .option("compression", "snappy")
-      .mode(SaveMode.Overwrite)
-      .format("parquet")
-      .save(path)
   }
 
   def getRddInfo(df: DataFrame) = {
